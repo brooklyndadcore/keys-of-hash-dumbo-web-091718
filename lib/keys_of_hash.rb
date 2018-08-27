366b919dcf8f924dc62224d2c50771b6561c01a4
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-27 20:08:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,13 +1,10 @@
 class Hash
   def keys_of(*arguments)
     keys = []
-    arguments.each do |arg| 
-      return arg
-      # arg.each |k, v| 
-      #   if val == arg
-      #     keys << key 
-      #   end
-      # end
+    arguments.each do |arg, val| 
+      if val == arg.values
+        keys << arg.keys
+      end
     end
   return keys
  end
